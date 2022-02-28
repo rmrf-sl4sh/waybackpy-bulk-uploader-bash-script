1f352e03d15b3bf7ab50b3913c16b956a83d4774
jdqw210 <79405840+jdqw210@users.noreply.github.com>
2022-02-28 05:02:44 -0500

    Create script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # waybackpy-bulk-uploader-bash-script
  Reads a text file for URLs and archives them to the Wayback Machine using waybackpy
+ waybackpy is required, see: https://github.com/akamhy/waybackpy
---
+++ wayback-bulk-uploader.sh
@@ -0,0 +1,30 @@
+#--!/bin/sh
+
+#--if user calls script incorrectly, let them know how to call it:
+if [ $# -ne 1 ];
+then
+	echo "[:] Usage: $0 URLs.txt";
+	exit -1
+fi
+#--ask user to confirm start of script, define variable $resp:
+echo ""
+read -p "[:] wayback machine bulk URL archive saver, shall we begin? (y/n) " resp
+echo ""
+#--if user enters 'y', begin loops to read/search tweets and save them:
+if [ "$resp" = "y" ]; then
+	#--define variable $lines as each line in a file $1, and start for loop:
+	lines=$(cat $1)
+	for line in $lines;
+		do
+			#--tell user which URL we are uploading and upload to the wayback machine:
+			echo "[:] Uploading $line now..."
+			waybackpy --url $line --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0" --save
+			#--the wayback API limits 15 urls/min upload, wait 5 seconds to accomodate this
+			echo "[:] waiting 5 seconds..."
+			sleep 5
+		done
+	echo "[:] completed."
+else
+	echo "[:] exiting..."
+	exit -1
+fi
